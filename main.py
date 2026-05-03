87d1aa801823bd1fa3502b939777a456b7e2bbcb
Diya1007 <diyaothers@gmail.com>
2026-05-02 21:48:13 -0400

    Change Flask app to run on host 0.0.0.0 and port 5001

--- main.py
+++ main.py
@@ -122,4 +122,4 @@ def search():
     return render_template("search.html", results=results)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=False)
